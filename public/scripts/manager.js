dde57410724153fc77056d1e1b69ed9087943a5b
BelmansGlenn <belmansglenn@gmail.com>
2021-06-17 17:17:35 +0200

    add table database and click event button day

--- public/scripts/manager.js
+++ public/scripts/manager.js
@@ -1,1 +1,21 @@
 // FRONT END FILE TO INTERACT WITH THE DOM
+const chooseDay = document.getElementById('chooseDay');
+const dayOfWeek = ["Monday","Tuesday","Wedn.","Thurs.","Friday","Saturday","Sunday"];
+let i = 0;
+let theDay = `<p>${dayOfWeek[i]}</p>`;
+chooseDay.addEventListener('click', (e) => {
+    chooseDay.innerHTML="";
+    e.preventDefault();
+    if(i<dayOfWeek.length-1){
+        i++;   
+        theDay = `<p>${dayOfWeek[i]}</p>`;
+        chooseDay.insertAdjacentHTML("beforeend", theDay);
+    }else{
+        i = 0;
+        theDay = `<p>${dayOfWeek[i]}</p>`;
+        chooseDay.insertAdjacentHTML("beforeend", theDay);
+    }
+    
+})
+
+chooseDay.insertAdjacentHTML("beforeend", theDay);

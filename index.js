e3ee518e45cb9ec12782013d85927a03ea608412
AyraStelmaszewski <ayrastelmaszewski@gmail.com>
2021-06-18 14:38:03 +0200

    oups

--- index.js
+++ index.js
@@ -16,10 +16,31 @@ app.get('/',(req, res) => {
   res.render('manager.ejs');
 });
 
-app.get('/planYourWeek',(req, res) => {
-  // callback
-  res.render('day.ejs');
+
+
+// All Day 
+app.get('/monday',(req, res) => {
+  res.render('monday.ejs');
 });
+app.get('/tuesday',(req, res) => {
+  res.render('tuesday.ejs');
+});
+app.get('/wednesday',(req, res) => {
+  res.render('wednesday.ejs');
+});
+app.get('/thursday',(req, res) => {
+  res.render('thursday.ejs');
+});
+app.get('/friday',(req, res) => {
+  res.render('friday.ejs');
+});
+app.get('/saturday',(req, res) => {
+  res.render('saturday.ejs');
+});
+app.get('/sunday',(req, res) => {
+  res.render('sunday.ejs');
+});
+
 
 app.get('/tasksOfTheDay',(req, res) => {
   // callback
@@ -32,6 +53,10 @@ app.post('/api/addtask', (req, res) => {
   manager.addingTask(req.body)
 });
 app.post('/api/loadtask', manager.displayingTask);
+
+
+
+
 
 
 
